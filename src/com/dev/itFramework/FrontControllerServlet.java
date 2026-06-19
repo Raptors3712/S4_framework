22969d8e8800e4be945ac3c11abd71e4b7c66b91
Ndroso <ndrosotianarivo@gmail.com>
2026-06-19 17:07:26 +0300

    sprint1
    3 fichiers

--- src/com/dev/itFramework/FrontControllerServlet.java
+++ src/com/dev/itFramework/FrontControllerServlet.java
@@ -7,8 +7,31 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.lang.Override;
+import java.util.ArrayList;
+import java.util.List;
+import com.dev.itFramework.utils.ListeControllerAnnote;
+import com.dev.itFramework.Controller;
+
 public class FrontControllerServlet extends HttpServlet
 {
+
+    private List<Class<?>> controllers;
+
+    @Override
+    public void init() throws ServletException {
+        String initTestControllers = this.getInitParameter("initTestController");
+        controllers = new ArrayList<>();
+        try {
+            for (String p : initTestControllers.split(";")) {
+                controllers.addAll(ListeControllerAnnote.getController(Controller.class, p));
+            }
+        }
+        catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
     {
@@ -30,8 +53,12 @@ public class FrontControllerServlet extends HttpServlet
         String path = request.getServletPath();
         String methode = request.getMethod();
 
-        out.println("OK sprint0!");
+        out.println("ETU004331");
+        out.println("OK sprint1!");
         out.println("Methode = " + methode);
         out.println("URL = " + path);
+        for (Class<?> controller : controllers) {
+            out.println(controller.getName());
+        }
     }
 }
